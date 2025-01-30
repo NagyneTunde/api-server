e4d5cb9b21b9e8840e5d75168304620d6fe5da80
Tünde Nagyné <nagy.gelencser@gmail.com>
2025-01-30 13:28:04 +0100

    set up test database

---
+++ app.js
@@ -0,0 +1,22 @@
+const todos = [
+  {
+    id: 1,
+    task: "reggelit készíteni",
+    completed: false,
+  },
+  {
+    id: 2,
+    task: "elindulni munkába",
+    completed: false,
+  },
+  {
+    id: 3,
+    task: "dolgozni",
+    completed: false,
+  },
+  {
+    id: 4,
+    task: "hazamenni munkából",
+    completed: false,
+  },
+];
