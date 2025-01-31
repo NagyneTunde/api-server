5f25cd9e1540f71a07cc5a72834c8421e5828465
Tünde Nagyné <nagy.gelencser@gmail.com>
2025-01-31 20:46:52 +0100

    todos api endpoint, filtering

--- app.js
+++ app.js
@@ -16,6 +16,17 @@ app.get("/test", (req, res) => {
   res.sendFile(`${__dirname}/public/test.html`);
 });
 
+//api response
+app.get("/todos", (req, res) => {
+  if (req.query.id !== undefined) {
+    const id = Number(req.query.id);
+    const searchedTodo = todos.filter((todo) => todo.id === id);
+    res.status(200).json(searchedTodo);
+  } else {
+    res.status(200).json(todos);
+  }
+});
+
 // szerver indítása
 app.listen(3000, () => {
   console.log(`Server is run on port 3000`);
