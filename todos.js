1cf963136773e0adb28d0cbc6c2e21aea50b6937
Tünde Nagyné <nagy.gelencser@gmail.com>
2025-01-31 19:17:42 +0100

    export todos

---
+++ todos.js
@@ -0,0 +1,25 @@
+const todos = [
+  {
+    id: 1,
+    task: "reggelit készíteni",
+    completed: false,
+  },
+  {
+    id: 2,
+    task: "elindulni munkába",
+    completed: false,
+  },
+  {
+    id: 3,
+    task: "dolgozni",
+    completed: false,
+  },
+  {
+    id: 4,
+    task: "hazamenni munkából",
+    completed: false,
+  },
+];
+
+// odo lista exportálása
+module.exports = todos;
